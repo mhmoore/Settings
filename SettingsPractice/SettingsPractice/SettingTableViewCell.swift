4125c244d2567784beac77e0f5397beb6aa87ecb
Mike Moore <michaelhmoore1@gmail.com>
2019-08-05 10:56:29 -0600

    creates setting tableView and tableView cell classes

---
+++ SettingsPractice/SettingsPractice/SettingTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SettingTableViewCell.swift
+//  SettingsPractice
+//
+//  Created by Michael Moore on 8/5/19.
+//  Copyright © 2019 Michael Moore. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    // MARK: - Outlets
+    
+    // MARK: - Properties
+    
+    var setting: Setting?
+    
+    // MARK - Custom Methods
+
+}
---
+++ SettingsPractice/SettingsPractice/SettingTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingTableViewController.swift
+//  SettingsPractice
+//
+//  Created by Michael Moore on 8/5/19.
+//  Copyright © 2019 Michael Moore. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SettingController.shared.settings.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath) as? SettingTableViewCell else { return UITableViewCell() }
+
+        let setting = SettingController.shared.settings[indexPath.row]
+        
+        cell.setting = setting
+        
+        return cell
+    }
+}
