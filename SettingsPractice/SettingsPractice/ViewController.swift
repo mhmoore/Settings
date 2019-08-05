39a76e220e318c18296a17e582493e4a856b001b
Mike Moore <michaelhmoore1@gmail.com>
2019-08-05 10:07:12 -0600

    initial commit

---
+++ SettingsPractice/SettingsPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SettingsPractice
+//
+//  Created by Michael Moore on 8/5/19.
+//  Copyright © 2019 Michael Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
